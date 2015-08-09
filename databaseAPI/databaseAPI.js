8cc631c991c69e65023d165faeb89affcbee4d0c
wo-amlangwang <adamlangwang@gmail.com>
2015-08-09 15:57:08 -0400

    databaseAPI for profile and user table

--- databaseAPI/databaseAPI.js
+++ databaseAPI/databaseAPI.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql');
+var Promise = require('Promise');
+var base ='heroku_1024a2f8499bceb';
 module.exports=function(){
   var buildConnection = function(){
     var connection = mysql.createConnection({
@@ -12,17 +14,97 @@ module.exports=function(){
         setTimeout(buildConnection,2000);
       }
     });
-    connection.on('error',function(err){
-      console.log('db error',err);
-      if(err.code === 'PROTOCOL_CONNECTION_LOST')
-      {
-        buildConnection();
-      }else{
-        throw err;
-      }
-    });
+    return connection;
   }
   return{
+    findUserById : function(id){
+      var ps = new Promise(function(fullfill,reject){
+        var connection = buildConnection();
+        var query = 'SELECT * FROM ' + base + '.user WHERE id LIKE' + ' \'' + id
+                    + '\'';
+        connection.query(query,function(err,rows,fields){
+          if(err){
+            reject(err);
+          }else{
+            fullfill(rows);
+          }
+        });
+        connection.end();
+      });
+      return ps;
+    },/**
+    data format{
+    email : string
+    hashed_pw : string
+    pwsalt : string
+    }
+    **/
+    insertNewUser : function(data){
+      var ps = new Promise(function(fullfill,reject){
+        var connection = buildConnection();
+        var query = 'INSERT INTO ' + base
+            + '.user (email,hashed_pw,pwsalt) VALUES ('+'\''+ data.email
+            +'\',\''+ data.hasehed_pw +'\',\''+ data.pwsalt +'\')';
+        connection.query(query,function(err,result,fields){
+          if(err){
+            reject(err);
+          }else{
+            fullfill(result);
+          }
+        });
+        connection.end();
+      });
+      return ps;
+    },
+    findProfileById : function(id){
+      var ps = new Promise(function(fullfill,reject){
+        var connection = buildConnection();
+        var query = 'SELECT * FROM ' + base + '.profile WHERE id LIKE' + ' \''
+                    + id + '\'';
+        connection.query(query,function(err,rows,fields){
+          if(err){
+            reject(err);
+          }else{
+            fullfill(rows);
+          }
+        });
+        connection.end();
 
+      });
+      return ps;
+    },
+    insertNewProfile : function(){
+      var ps = new Promise(function(fullfill,reject){
+        var connection = buildConnection();
+        var query = 'INSERT INTO ' + base
+            + '.profile (last_name,first_name) VALUES (NULL,NULL)';
+        connection.query(query,function(err,result,fields) {
+          if(err){
+            reject(err);
+          }else{
+            fullfill(result);
+          }
+        });
+        connection.end();
+      });
+      return ps;
+    },
+    connectProfileWithUser : function(uid,pid){
+      var ps = new Promise(function(fullfill,reject){
+        var connection = buildConnection();
+        var query = 'INSERT INTO ' + base
+            + '.profile_user_links (user_id,profile_id) VALUES ('+'\''+ uid
+            +'\',\''+ pid +'\''+')';
+        connection.query(query,function(err,result,fields) {
+          if(err){
+            reject(err);
+          }else{
+            fullfill(result);
+          }
+        });
+        connection.end();
+      });
+      return ps;
+    }
   }
 }
---
+++ databaseAPI/test.js
@@ -0,0 +1,30 @@
+var database = require('./databaseAPI.js')();
+
+
+database.insertNewUser({'email' : 'langwang@gmail.com','hasehed_pw' : '12345','pwsalt':'321'}).then(function(result){
+  console.log(result);
+}).catch(function(err){
+  console.log(err);
+});
+database.insertNewProfile().then(function(result){
+  console.log(result);
+}).catch(function(err){
+  console.log(err);
+});
+/**
+database.findUserById(1422).then(function(result){
+  console.log(result);
+}).catch(function(err){
+  console.log(err);
+});
+
+database.findProfileById(102).then(function(result){
+  console.log(result);
+}).catch(function(err){
+  console.log(err);
+});
+database.connectProfileWithUser(1,2).then(function(result){
+  console.log(result);
+}).catch(function(err){
+  console.log(err);
+});**/
