7fd4634b48e08e77bf65bfacedd335e718a0cca9
wo-amlangwang <adamlangwang@gmail.com>
2015-08-19 01:48:57 -0400

    give sqlpool more connection

--- models/databaseAPI/sqlpool.js
+++ models/databaseAPI/sqlpool.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 module.exports = {
   pool : mysql.createPool({
-    connectionLimit : 1,
+    connectionLimit : 10,
     host      : 'us-cdbr-iron-east-02.cleardb.net',
     user      : 'bb073b07f5136c',
     password  : '9a31dae2'
