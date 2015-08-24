3d1f0dd91a3b7d9570f68dab734f0e6413ad9d45
Siyuan Gao <siyuangao@gmail.com>
2015-08-24 16:37:14 -0700

    Update sqlpool.js

--- modules/databaseAPI/sqlpool.js
+++ modules/databaseAPI/sqlpool.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 module.exports = {
+  // Siyuan: 请使用node ENV环境变量，不要把登录信息存在文件里
   pool : mysql.createPool({
     connectionLimit : 3,
     host      : 'us-cdbr-iron-east-02.cleardb.net',
