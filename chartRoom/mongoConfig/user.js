bb374fc1bb7fbbf9d40daddb8681afc9ca227fca
wo-amlangwang <adamlangwang@gmail.com>
2015-09-28 16:52:34 -0400

    self local login

--- chartRoom/mongoConfig/user.js
+++ chartRoom/mongoConfig/user.js
@@ -2,11 +2,9 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 
 var userSchema = mongoose.Schema({
-  local : {
-    email : String,
-    password : String,
-    currentUuid : String
-  }
+  email : String,
+  password : String,
+  currentUuid : String
 });
 
 userSchema.methods.generateHash = function(password) {
--- chartRoom/server.js
+++ chartRoom/server.js
@@ -5,9 +5,32 @@ var io = require('socket.io')(server);
 var port = process.env.PORT || 4000;
 var bodyParser = require('body-parser');
 var uuid = require('node-uuid');
-mongoose.connect();
+var User = require('./mongoConfig/user.js');
+
+mongoose.connect('mongodb://dalangwang:1234qwer@ds035593.mongolab.com:35593/heroku_qf7gd7t4',function(err) {
+  if(err){
+    console.log(err);
+    process.exit(0);
+  }else {
+    console.log('connect to database');
+  }
+});
 
 app.use(bodyParser());
+app.post('/ivory/chartroom/register',function(req,res) {
+  User.findOne({'email' : req.body.eamil},function(err, user){
+    if(err){
+      console.log(err);
+      res.status(503).send('database err');
+    }else {
+      var newUser = new User();
+      newUser.email = email;
+      newUser.password = newUser.generateHash(password);
+      res.status(200).send('OK');
+    }
+  });
+});
+
 app.post('/ivory/chartroom/login',function(req,res) {
   var client;
   if(req.body.isGuest === 'true'){
